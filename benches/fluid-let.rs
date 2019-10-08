9f975d5e98310e0d44eae757c5f037eff235e1c7
Alexei Lozovsky <me@ilammy.net>
2019-10-08 18:55:40 +0300

    Benchmarks for dynamic variable access (#5)
    * Benchmarks for dynamic variable access
    
    Add some primitive benchmarks for variable access. I came to like the
    criterion microbenchmarking framework so let's use it. Since the crate
    does not have much API surface to cover, we measure the overhead for
    accessing a dynamic variable for reading and writing. Difference between
    the benchmarks for static and dynamic is the overhead spent on thread-
    local storage and all that's in-between.
    
    The overhead turns out to be relatively small: around 5 nanoseconds on
    my machine. However, it is also quite large in the perspective: static
    variable access takes about 0.5 nanoseconds, therefore dynamic variables
    introduce about x10 slowdown.
    
    * Separate build and run steps
    
    Well... Benchmarking requires criterion crate which pull in quite
    a few additional development dependencies. Since benchmarks are
    using the same --test configuration as tests, all this stuff gets
    built even when we run "cargo test" and that takes a lot of time.
    
    Let's split the building and running parts of tests so that the CI
    report looks more useful and accurately reflects time spend on
    running our tests, as opposed to building other people's code.

---
+++ benches/fluid-let.rs
@@ -0,0 +1,52 @@
+// Copyright (c) 2019, ilammy
+// Licensed under MIT license (see LICENSE)
+
+use criterion::*;
+
+use fluid_let::fluid_let;
+
+fn get(c: &mut Criterion) {
+    let mut group = c.benchmark_group("fluid_let");
+    group.bench_function(BenchmarkId::new("get", "dynamic"), |b| {
+        fluid_let!(static COUNTER: i32);
+        let mut total = 0;
+        COUNTER.set(1, || {
+            b.iter(|| COUNTER.get(|value| total += value.unwrap_or(&0)));
+        });
+    });
+    group.bench_function(BenchmarkId::new("get", "static"), |b| {
+        static mut COUNTER: Option<&i32> = None;
+        let mut total = 0;
+        unsafe {
+            COUNTER = Some(&1);
+            b.iter(|| total += COUNTER.unwrap_or(&0));
+            COUNTER = None;
+        }
+    });
+    group.finish();
+}
+
+fn set(c: &mut Criterion) {
+    let mut group = c.benchmark_group("fluid_let");
+    group.bench_function(BenchmarkId::new("set", "dynamic"), |b| {
+        fluid_let!(static COUNTER: i32);
+        let mut total = 0;
+        b.iter(|| COUNTER.set(total, || total += total));
+    });
+    group.bench_function(BenchmarkId::new("set", "static"), |b| {
+        static mut COUNTER: Option<&i32> = None;
+        let mut total = 0;
+        b.iter(|| unsafe {
+            // It's safe to transmute reference lifetime like this:
+            COUNTER = Some(std::mem::transmute(&total));
+            total += total;
+            black_box(());
+            COUNTER = None;
+        });
+    });
+    group.finish();
+}
+
+criterion_group!(fluid_let, get, set);
+
+criterion_main!(fluid_let);
